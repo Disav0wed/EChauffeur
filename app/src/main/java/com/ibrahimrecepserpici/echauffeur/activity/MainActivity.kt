f0d0a97a37b95aa6cc6e94a13f8d84aeaef6a573
Ibrahim R Serpici <ibrahimrecepserpici@gmail.com>
2021-11-28 13:28:21 +0300

    feat: Bottom Navigation Bar implemented with map and list fragments.

--- app/src/main/java/com/ibrahimrecepserpici/echauffeur/activity/MainActivity.kt
+++ app/src/main/java/com/ibrahimrecepserpici/echauffeur/activity/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.ibrahimrecepserpici.echauffeur.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ibrahimrecepserpici.domain.entity.Coordinate
 import com.ibrahimrecepserpici.echauffeur.R
 import com.ibrahimrecepserpici.echauffeur.viewmodel.TaxiViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+
+
+
 @AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
 
     private val taxiViewModel: TaxiViewModel by viewModels()
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
 
         taxiViewModel.getVehicleInformations(
@@ -26,5 +40,27 @@ class MainActivity: AppCompatActivity() {
 
 
 
+        navController = findNavController(R.id.nav_host_fragment)
+        initBottomNavMenu(navController)
+
     }
+
+    private fun initBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+
+        bottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_nav_menu_item_list_fragment -> navController.navigate(R.id.action_global_listFragment)
+                R.id.bottom_nav_menu_item_map_fragment -> navController.navigate(R.id.action_global_mapFragment)
+            }
+            true
+        }
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/ibrahimrecepserpici/echauffeur/viewmodel/TaxiViewModel.kt
+++ app/src/main/java/com/ibrahimrecepserpici/echauffeur/viewmodel/TaxiViewModel.kt
@@ -4,22 +4,24 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ibrahimrecepserpici.domain.entity.Coordinate
+import com.ibrahimrecepserpici.domain.entity.VehicleInfo
 import com.ibrahimrecepserpici.domain.usecase.vehicle.IGetVehicleInfosUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltViewModel
 class TaxiViewModel @Inject constructor(private val getVehicleInfoUseCase: IGetVehicleInfosUseCase) :
     ViewModel() {
 
-         fun getVehicleInformations(point1: Coordinate, point2: Coordinate){
-            GlobalScope.launch(Dispatchers.IO) {
-                val infos = getVehicleInfoUseCase.getVehicleInfosInRegion(point1,point2)
-                Log.e("TEST",infos.get(0).fleetType)
+         fun getVehicleInformations(point1: Coordinate, point2: Coordinate) : Deferred<Result<List<VehicleInfo>>>{
+            return GlobalScope.async {
+                kotlin.runCatching {
+                    getVehicleInfoUseCase.getVehicleInfosInRegion(point1,point2)
+                }
             }
         }
+
+
 
 }
